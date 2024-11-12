04958a8cef9ac28c16e91070a7de98e7f6271b2d
tiway <stilon670@gmail.com>
2024-11-12 19:02:59 +0200

    Added CategoryAdmin

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import CategoryDB
 
-# Register your models here.
+
+@admin.register(CategoryDB)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ["id", "title", "parent", "slug"]
+    list_display_links = ["id", "title"]
+    prepopulated_fields = {"slug": ("title",)}
