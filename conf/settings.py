9f66155615a1b8e646613a982ea185795cbc7ee5
tiway <stilon670@gmail.com>
2024-11-23 15:42:41 +0200

    Implemented sending messages to email for everyone who is subscribed

--- conf/settings.py
+++ conf/settings.py
@@ -157,3 +157,12 @@ AUTH_USER_MODEL = "users.User"
 COMPRESS_PRECOMPILERS = [
     ("text/x-scss", "sass {infile} {outfile}"),
 ]
+
+# Email
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = os.getenv("EMAIL_HOST")
+EMAIL_PORT = os.getenv("EMAIL_PORT")
+EMAIL_USE_TLS = bool(int(os.getenv("EMAIL_USE_TLS")))
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
--- notifications/views.py
+++ notifications/views.py
@@ -1,12 +1,13 @@
 from django.db.utils import IntegrityError
 from django.http import HttpRequest, HttpResponse
-from django.shortcuts import redirect
+from django.shortcuts import redirect, render
 from django.contrib import messages
 
 from notifications.models import Subscriber
 
 
 def save_subscribers(request: HttpRequest) -> HttpResponse:
+    """Зберігаємо email"""
     email = request.POST.get("email")
     user = request.user if request.user.is_authenticated else None
     if email:
@@ -15,3 +16,27 @@ def save_subscribers(request: HttpRequest) -> HttpResponse:
         except IntegrityError:
             messages.error(request, "Такий email вже підписан")
     return redirect("shop:index")
+
+
+def send_email_to_subscribers(request: HttpRequest) -> HttpResponse:
+    """Відправка розсилки всім"""
+    from django.core.mail import send_mail
+    from conf import settings
+
+    if request.method == "POST":
+        title = request.POST.get("title_send_email")
+        text = request.POST.get("text_send_email")
+        subscriber = Subscriber.objects.all()
+        emails = [i.email for i in subscriber]
+        send_mail(
+            title,
+            text,
+            settings.EMAIL_HOST_USER,
+            emails,
+            fail_silently=False,
+        )
+        print(f"Відправлено все --- {bool(send_mail)}")
+
+    context = {"title": "Спамер"}
+
+    return render(request, "shop/send_email.html", context)
