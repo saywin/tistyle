2efdc384a35f3c3b8c0743ea1ad4b27d3d50ea62
tiway <stilon670@gmail.com>
2024-11-26 17:45:58 +0200

    Create func checkout for app order to vies.py

--- order/views.py
+++ order/views.py
@@ -1,3 +1,21 @@
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+from cart.utils import CartForAuthenticatedUser
+from order.forms import ShippingForm
+from users.forms import CustomerForm
+
+
+def checkout(request: HttpRequest) -> HttpResponse:
+    """Сторінка оформлення замовлення"""
+    cart_info = CartForAuthenticatedUser(request).get_cart_info()
+    context = {
+        "title": "Оформлення замовлення",
+        "cart": cart_info["cart"],
+        "products_to_cart": cart_info["products_to_cart"],
+        "cart_total_price": cart_info["cart_total_price"],
+        "cart_total_quantity": cart_info["cart_total_quantity"],
+        "customer_form": CustomerForm(),
+        "shipping_form": ShippingForm(),
+    }
+    return render(request, "shop/checkout.html", context)
