97e26e2d4fc0841d7808116e404f14938fbc5fe3
tiway <stilon670@gmail.com>
2024-11-26 17:45:28 +0200

    Create func cart and add_to_cart for app cart to vies.py

--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from django.http import HttpResponse, HttpRequest
+from django.shortcuts import render, redirect
+from django.contrib import messages
 
-# Create your views here.
+from cart.utils import CartForAuthenticatedUser
+
+
+def cart(request: HttpRequest) -> HttpResponse:
+    cart_info = CartForAuthenticatedUser(request).get_cart_info()
+    context = {
+        "title": "Кошик",
+        "cart": cart_info["cart"],
+        "products_to_cart": cart_info["products_to_cart"],
+        "cart_total_price": cart_info["cart_total_price"],
+        "cart_total_quantity": cart_info["cart_total_quantity"],
+    }
+    return render(request, "shop/cart.html", context)
+
+
+def add_to_cart(
+    request: HttpRequest, product_id: int, size_id: int, action: str
+) -> HttpResponse:
+    if request.user.is_authenticated:
+        CartForAuthenticatedUser(
+            request=request,
+            size_id=size_id,
+            product_id=product_id,
+            action=action,
+        )
+        return redirect("cart:user_cart")
+    else:
+        messages.error(
+            request, "Треба авторизуватись або зареєструватись, щоб завершити покупки"
+        )
+        return redirect("users:login")
